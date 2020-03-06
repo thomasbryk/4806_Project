f7faa86427272ec802b324ea8cff04919a65e7e8
Thomas Bryk <thomasbryk@cmail.carleton.ca>
2020-03-05 20:20:45 -0500

    Added order repository functions

--- src/main/java/repositories/OrderRepository.java
+++ src/main/java/repositories/OrderRepository.java
@@ -1,5 +1,6 @@
 package repositories;
 
+import models.Customer;
 import models.Order;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -8,5 +9,6 @@ import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "orders", path = "orders")
 public interface OrderRepository extends PagingAndSortingRepository<Order, Integer> {
-
+    Order findById(long id);
+    List<Order> findByCustomer (Customer customer);
 }
