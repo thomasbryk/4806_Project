6cf4951d89863c8cd3b5fbe7b92cd3754d51072c
4iner <foreigncode@gmail.com>
2020-03-20 12:47:40 -0400

    Update redis config

--- src/main/java/security/SessionConfig.java
+++ src/main/java/security/SessionConfig.java
@@ -1,6 +1,8 @@
 package security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
@@ -10,4 +12,9 @@ public class SessionConfig extends AbstractHttpSessionApplicationInitializer{
     /**
      * Session configuration for Redis
      */
+
+    @Bean
+    public static ConfigureRedisAction configureRedisAction() {
+         ConfigureRedisAction.NO_OP;
+    }
 }
