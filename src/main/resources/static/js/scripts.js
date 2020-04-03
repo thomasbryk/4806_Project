bcec92e02a0bea9ec627f233fef1800890b60242
thomasbryk <thomasbryk@cmail.carleton.ca>
2020-04-03 02:40:33 -0400

    Separated styles and scripts into their own files.

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,13 +1,25 @@
-
-
-
-
-
-
+//Parses URL to obtain value from given parameter name
 urlParam = function(name){
     var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
     if (results==null) {
        return null;
     }
     return decodeURI(results[1]) || 0;
-}
+};
+
+
+$.fn.serializeObject = function () {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function () {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
